3fd50262f493249eaf0354140489690f6403fba2
Yab <yeabalex18@gmail.com>
2025-01-22 04:08:45 -0800

    proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://blogserver.yeabsiraa.com/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
